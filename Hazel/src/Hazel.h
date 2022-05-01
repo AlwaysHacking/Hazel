5df1048a15b220aad19805eb4710a66e09ee6030
Liu Haitian <i@liu.ht>
2022-05-01 22:58:10 +0800

    Initial

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Hazel/Application.h"
+
+#include "Hazel/Log.h"
+
+// --- Entry Point-------------------
+#include "Hazel/EntryPoint.h"
+// ----------------------------------
---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,27 @@
+#include "hzpch.h"
+
+#include "Application.h"
+#include "Hazel/Events/ApplicationEvent.h"
+#include <GLFW/glfw3.h>
+
+namespace Hazel {
+	Application::Application()
+	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
+	}
+
+	Application::~Application()
+	{
+	}
+
+	void Application::Run()
+	{
+		while (m_Running)
+		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
+		}
+	}
+
+}
