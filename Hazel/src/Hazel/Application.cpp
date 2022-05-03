23b12d5063754a38367a0c9905fab098bab9c07c
Liu Haitian <i@liu.ht>
2022-05-04 02:20:20 +0800

    feat: add window events

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,17 +1,27 @@
 #include "hzpch.h"
 
 #include "Application.h"
-#include "Hazel/Events/ApplicationEvent.h"
 #include <GLFW/glfw3.h>
 
 namespace Hazel {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispacter(e);
+		dispacter.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		HZ_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -22,6 +32,12 @@ namespace Hazel {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }
